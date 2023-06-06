c2bf0fbb0e7117dc51522eac30ff2c1c1ed327f6
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-06-05 19:26:11 -0500

    [ADD] add movie feature :zap:

---
+++ src/functions/addMovie/addMovie.ts
@@ -0,0 +1,36 @@
+import { v4 } from 'uuid'
+import AWS from 'aws-sdk'
+import { IAddMovie } from '@functions/health/core/interface/addMovie.interface';
+
+export const handler = async (event: any) => {
+
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+    const { title, releaseYear, director, genre, durationMinutes, rating, description } = JSON.parse(event.body) as IAddMovie;
+    const createdAt = new Date();
+    const id = v4();
+
+    const newMovie = {
+        id,
+        title,
+        releaseYear,
+        director,
+        genre,
+        durationMinutes,
+        rating,
+        description,
+        createdAt
+    }
+
+    await dynamodb.put({
+        TableName: 'MovieTable',
+        Item: newMovie
+    }).promise()
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(newMovie)
+    }
+
+}
+
